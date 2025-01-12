c6b9071c6cb91348f9049462200e47829d8b9fb4
Ali Akbar <116874053+aliakbar-deriv@users.noreply.github.com>
2025-01-12 19:15:53 +0400

    Vercel deployment configs (#4)
    * Vercel deployment configuration added.
    
    * Updated actions version and node version
    
    * Updated dependencies version to the latest and fixed lint errors
    
    * removed redundant trigger
    
    * Optimized workflow
    
    * Improved and optimized actions and workflows
    
    * Quality check workflow fixes
    
    * workflow jobs separated
    
    * Added and refactored workflows

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, '.')
 
   return {
     plugins: [react()],
